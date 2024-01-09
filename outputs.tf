b79130261ed7bf815790f1d04a87e3129b896afa
Ana Custura <ana@sr2.uk>
2024-01-09 09:54:02 +0000

    Add monitoring service values to module outputs

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,14 @@ output "tunnel_secret" {
   value = cloudflare_argo_tunnel.default.secret
 }
 
+output "access_client_id" {
+  value = cloudflare_access_service_token.monitoring.client_id
+}
+
+output "access_client_secret" {
+  value = cloudflare_access_service_token.monitoring.client_secret
+}
+
 output "metamigo_audience" {
   value = cloudflare_access_application.default[local.metamigo_domain].aud
 }
